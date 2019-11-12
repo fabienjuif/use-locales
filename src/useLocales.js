abd075063e4d55d9f0b1ef8abe479ae5823f15e4
Fabien JUIF <fabien.juif@gmail.com>
2019-11-12 11:06:09 +0100

    :bug: do not set state twice if object is null as instanciation

--- src/useLocales.js
+++ src/useLocales.js
@@ -1,13 +1,18 @@
-import { useContext, useState, useLayoutEffect } from 'react'
+import { useContext, useState, useLayoutEffect, useRef } from 'react'
 import { LocalesContext } from './context'
 import format from './messageFormat'
 
 export const useLocales = (path, object) => {
+  const firstDraw = useRef(true)
   const { getMessages, lang } = useContext(LocalesContext)
   const [messages, setMessages] = useState(getMessages(path))
 
   useLayoutEffect(() => {
     const rawMessage = getMessages(path)
+
+    if (firstDraw.current && !object) return
+    firstDraw.current = false
+
     if (!object) {
       setMessages(rawMessage)
       return
